56fc4184fafea60395a3d110575216b09499f482
Gustavo Morais <gustvmorais@gmail.com>
2023-09-01 08:41:53 -0300

    Finalizando o CRUD do usuario

--- src/main/java/tech/gustvmorais/criandoAPI/Controller/UsuarioController.java
+++ src/main/java/tech/gustvmorais/criandoAPI/Controller/UsuarioController.java
@@ -1,20 +1,39 @@
 package tech.gustvmorais.criandoAPI.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.gustvmorais.criandoAPI.DAO.IUsuario;
 import tech.gustvmorais.criandoAPI.model.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
-    @GetMapping("/usuario")
+    @GetMapping
     public List<Usuario> Listausuario(){
         return (List<Usuario>) dao.findAll();
+    }
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
+         Usuario novoUsuario = dao.save(usuario);
+         return novoUsuario;
+    }
+
+    @PutMapping
+    public Usuario alterarUsuario (@RequestBody Usuario usuario){
+        Usuario novoUsuario = dao.save(usuario);
+        return novoUsuario;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> deletarUsuario (@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
     }
 }
